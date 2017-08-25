da4ebb78ec91479e00dbc777a500b4e2eab24b59
ABaldrati <1fbaldratialberto@gmail.com>
2017-08-25 17:42:00 +0200

    Terza versione con crrezzione bug: Prodotto,somma, trasposta funzinano correttamente

--- MatrixTemplate.h
+++ MatrixTemplate.h
@@ -7,75 +7,97 @@
 
 #include <iostream>
 
-template <typename T>
+template<typename T>
 class MatrixTemplate {
 public:
-    MatrixTemplate(int r, int c):rows(r), columns(c){
-        buffer=new T[r*c];
+    MatrixTemplate(int r, int c) : rows(r), columns(c) {
+        buffer = new T[r * c];
     }
 
-    ~MatrixTemplate(){
-        delete [] buffer;
+    ~MatrixTemplate() {
+        delete[] buffer;
     }
 
-    MatrixTemplate& operator+=(const MatrixTemplate& rh){
-        for(int i=0;i<rows*columns;i++)
-            buffer[i]=buffer[i]+rh.buffer[i];
+    MatrixTemplate(const MatrixTemplate &rh) {
+        rows = rh.rows;
+        columns = rh.columns;
+        buffer = new T[rows * columns];
+        for (int i = 0; i < rows * columns; i++)
+            buffer[i] = rh.buffer[i];
+    }
+
+    MatrixTemplate &operator=(const MatrixTemplate &rh) {
+        if (&rh == this)
+            return *this;
+        rows = rh.rows;
+        columns = rh.columns;
+        delete[]buffer;
+        buffer = new T[rows * columns];
+        for (int i = 0; i < rows * columns; i++)
+            buffer[i] = rh.buffer[i];
+
+    }
+
+
+    MatrixTemplate &operator+=(const MatrixTemplate &rh) {
+        for (int i = 0; i < rows * columns; i++)
+            buffer[i] = buffer[i] + rh.buffer[i];
         return *this;
     }
 
-    MatrixTemplate& operator+(const MatrixTemplate& lh, const MatrixTemplate& rh){
-        //TODO righe e colonne devono essere uguali
-        buffer=new T[lh.rows*lh.columns];
-        for(int i=0;i<rh.rows*rh.columns;i++)
-            buffer[i]=rh.buffer[i]+lh.buffer[i];
-        return *this;
+    MatrixTemplate operator+(const MatrixTemplate& rh) const{
+         //TODO righe e colonne devono essere uguali
+        MatrixTemplate<T> tmp(rows,columns);
+         for(int i=0;i<rh.rows*rh.columns;i++)
+             tmp.buffer[i]=buffer[i]+rh.buffer[i];
+         return tmp;
+     }
+
+    MatrixTemplate operator*(const MatrixTemplate& rh)const{
+         //TODO lh.colum==rh.rows
+        MatrixTemplate<T> tmp(rows,rh.columns);;
+         for(int i=0;i<rows;i++)
+             for(int j=0;j<rh.columns;j++){
+                 tmp.buffer[i*rh.columns+j]=0;
+                 for(int h=0;h<columns;h++)
+                     tmp.buffer[i*rh.columns+j]+=buffer[i*columns+h]*rh.buffer[h*rh.columns+j];
+             }
+         return tmp;
+     }
+
+    MatrixTemplate transpose ()const {
+        MatrixTemplate<T> tmp(columns,rows);
+        for (int i = 0; i < rows; i++)
+            for (int j = 0; j < columns; j++)
+                tmp.buffer[tmp.columns * j + i] = buffer[columns * i + j];
+        return tmp;
     }
 
-    MatrixTemplate& operator*(const MatrixTemplate& lh, const MatrixTemplate& rh){
-        //TODO lh.colum==rh.rows
-        buffer=new T[lh.rows*rh.columns];
-        for(int i=0;i<lh.rows;i++)
-            for(int j=0;j<rh.columns;j++){
-                buffer[i+rh.columns]=0;
-                for(int h=0;h<lh.columns;h++)
-                    buffer[i+rh.columns]+=lh.buffer[i*lh.columns+h]*rh.buffer[h*rh.columns+j];
+
+    void print() {
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                std::cout << buffer[columns * (i) + j] << " ";
             }
-        return *this;
-    }
-
-    MatrixTemplate& transpose(const MatrixTemplate& rh){
-        buffer=new T[rh.columns*rh.rows];
-        for(int i=0;i<rh.rows;i++)
-            for(int j=0;j<rh.columns;j++)
-                buffer[rh.columns*i+j]=rh.buffer[rh.columns*j+i];
-        return *this;
-    }
-
-
-    void print(){
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<columns;j++){
-                std::cout<<buffer[columns*i+j];
-            }
-            std::cout<<std::endl;
+            std::cout << std::endl;
         }
+        std::cout << std::endl;
     }
 
-    T getValue(int i,int j){
-       //TODO impostare i range gisuti
-        return buffer[columns*(j-1)+i-1];
+    T getValue(int i, int j) const {
+        //TODO impostare i range gisuti
+        return buffer[columns * (i - 1) + j - 1];
     }
 
-    void setValue(int i, int j,const T& value){
+     void setValue(int i, int j, const T &value) const {
         //TODO impostare i range corretti
-        buffer[columns*(j-1)+i-1]=value;
+        buffer[columns * (i - 1) + j - 1] = value;
     }
 
 
 private:
     int rows, columns;
-    T* buffer;
+    T *buffer;
 };
 
 
--- main.cpp
+++ main.cpp
@@ -1,6 +1,46 @@
 #include <iostream>
+#include "MatrixTemplate.h"
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    MatrixTemplate<int> MatA(2,3);
+    MatA.setValue(1,1,1);
+    MatA.setValue(1,2,2);
+    MatA.setValue(1,3,5);
+    MatA.setValue(2,1,12);
+    MatA.setValue(2,2,7);
+    MatA.setValue(2,3,7);
+    MatA.print();
+    /*MatrixTemplate<int> MatB(2,3);
+    MatB.setValue(1,1,4);
+    MatB.setValue(1,2,1);
+    MatB.setValue(2,1,5);
+    MatB.setValue(2,2,4);
+    MatB.setValue(1,3,5);
+    MatB.setValue(2,3,3);
+    MatB.print();
+    MatrixTemplate<int> MatD(2,3);
+    MatD.setValue(1,1,2);
+    MatD.setValue(1,2,8);
+    MatD.setValue(2,1,2);
+    MatD.setValue(2,2,1);
+    MatD.setValue(1,3,3);
+    MatD.setValue(2,3,7);
+    MatD.print();
+    MatrixTemplate<int> MatC(2,3);
+    MatC=MatA+MatB+MatD;
+    MatA.print();
+    MatB.print();
+    MatC.print();*/
+    MatrixTemplate<int> MatC(2,3);
+    MatC=MatA.transpose();
+    MatA.print();
+    MatC.print();
+    MatrixTemplate<int> MatD(2,2);
+    MatD=MatA*MatC;
+    MatD.print();
+
+
+
+
 }
